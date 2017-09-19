2033548a8666374e40bff9af60ceab9a8116e0a3
VerenaN <verena.naujoks@gmx.net>
2017-09-19 20:19:29 +0200

    Leveldesign
    Erster Stand vorm Zusammenführen

---
+++ Assets/Scripts/BGScaler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScaler : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        // Höhe berechen
+        float height = Camera.main.orthographicSize * 2f;
+        // Breite berechnen
+        float width = height * Screen.width / Screen.height;
+
+        if (gameObject.name == "Background")
+        {
+            // GameObject (Background) skalieren
+            transform.localScale = new Vector3(width, height, 0);
+        }
+        else
+            transform.localScale = new Vector3(width + 3f, 2, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour {
+
+    public LevelManager levelManager;
+
+    private void Start() {
+        levelManager = FindObjectOfType<LevelManager>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Collector") {
+            // Zeit speichern
+            if (levelManager.highscore > PlayerPrefs.GetFloat("CampusRunnerHighScore" + levelManager.levelName)) {
+                PlayerPrefs.SetFloat("CampusRunnerHighScore" + levelManager.levelName, levelManager.highscore);
+            }
+
+            // neustarten oder kleines Menü anzeigen
+            RestartGame ();
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D other) {
+        // Kollision mit Gegner
+		//if (other.gameObject.tag == "Gegner") {
+		//	// Spiel ist zu Ende
+		//	RestartGame();
+		//}
+	}
+
+	void RestartGame() {
+		// Spiel ist zu Ende
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+
+
+
+}
